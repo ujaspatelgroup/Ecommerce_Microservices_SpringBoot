3e74c6799740154c2da6173fbc326c86aab6dd9a
Ujas Patel <ujaspatelgroup@gmail.com>
2024-07-25 14:57:51 -0400

    Add Resilience4J

--- order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -27,7 +27,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -53,6 +53,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order placed Successfully";
         }else {
             throw new IllegalArgumentException("Product is not in stock");
         }
